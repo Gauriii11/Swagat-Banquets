7892e574962b9fd75d879eba4f6b549cf5bca6d7
Gauri Shinde <92216617+Gauriii11@users.noreply.github.com>
2023-11-30 21:51:09 +0530

    Add files via upload

---
+++ admin/gallery.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+
+<?php include_once('includes/header.php');?>
+  
+<?php include_once('includes/footer.php');?>
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Codebase Core JS -->
+        <script src="assets/js/core/jquery.min.js"></script>
+        <script src="assets/js/core/popper.min.js"></script>
+        <script src="assets/js/core/bootstrap.min.js"></script>
+        <script src="assets/js/core/jquery.slimscroll.min.js"></script>
+        <script src="assets/js/core/jquery.scrollLock.min.js"></script>
+        <script src="assets/js/core/jquery.appear.min.js"></script>
+        <script src="assets/js/core/jquery.countTo.min.js"></script>
+        <script src="assets/js/core/js.cookie.min.js"></script>
+        <script src="assets/js/codebase.js"></script>
+    </body>
+</html>
